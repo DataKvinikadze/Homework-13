edd9857797ec07b4560dcc219d0b2029294d45de
DataKvinikadze <datakvinikadze07@gmail.com>
2024-06-01 23:21:12 +0400

    broski

--- js/script.js
+++ js/script.js
@@ -1,9 +1,28 @@
-let url = "https://api.github.com/users/mind1a";
+// getting dark/light button
+
+let dark_lightButton = document.querySelector(".dark_light");
+
+// setting theme
+
+localStorage.setItem("theme", "light");
+
+dark_lightButton.addEventListener("click", () => {
+  if (localStorage.getItem("theme") == "light") {
+    localStorage.setItem("theme", "dark");
+  } else if (localStorage.getItem("theme") == "dark") {
+    localStorage.setItem("theme", "light");
+  }
+
+  if (localStorage.getItem("theme") == "dark") {
+  }
+});
+
+let url = "https://api.github.com/users";
 
 fetch(url, { method: "GET" })
   .then((response) => {
-    response.json();
+    return response.json();
   })
   .then((data) => {
-    console.log(data);
+    console.log();
   });
